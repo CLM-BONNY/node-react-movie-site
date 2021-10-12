ab2898e5e72fbdf7be73b253fb1348a37eec368f
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-10-12 16:41:43 +0900

    Create Config.js

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '7e628dbb2cfe798e5589a98d145bb84a';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
