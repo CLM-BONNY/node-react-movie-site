63b1b59abba84506244f809b8952880810db20ed
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-10-13 23:57:40 +0900

    Update favorite.js

--- sever/routes/favorite.js
+++ sever/routes/favorite.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { Favorite } = require('../models/Favorite');
 
-router.post('favoriteNumber', (req, res) => {
+router.post('/favoriteNumber', (req, res) => {
     // mongoDB에서 favorite 숫자 가져오기
-    Favorite.find({ "movieId": req.movieId})
+    Favorite.find({ "movieId": req.body.movieId})
         .exec((err, info) => {
             if (err) return res.status(400).send(err)
             // 그 다음에 프론트에 숫자 정보 보내주기
