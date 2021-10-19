fecbc528f51d6393efb77ec7a9c91280f192c521
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-10-19 11:48:11 +0900

    Update GridCards.js

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -6,7 +6,7 @@ function GridCards(props) {
         return (
             <Col lg={6} md={8} xs={24}>
                 <div style={{ position: 'relative' }}>
-                    <a herf={`/movie/${props.movieId}`}>
+                    <a href={`/movie/${props.movieId}`}>
                         <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName}/>
                     </a>
                 </div>
